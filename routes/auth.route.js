5ccd65015df1ed687f2e941da351972b8c0afb3d
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-10 11:19:27 +0100

    Rename controller folder to controllers

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup } = require('../controller/auth.controller');
+const { signup } = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.get('/signup', signup);
