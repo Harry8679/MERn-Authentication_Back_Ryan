98ae3b3151e5702610cfcc45dca1a0cb0adb90f4
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-10 12:03:41 +0100

    Connection to Mongo DB

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+dotenv.config();
+
 const app = express();
 
-dotenv.config();
+mongoose.connect(process.env.MONGO_URI, {})
+            .then(() => console.log('DB Connected'))
+            .catch((err) => console.log('DB Error => ', err));
+
 
 // import routes
 const authRoutes = require('./routes/auth.route');
+
+// app middlewares
+app.use(morgan('dev'));
+if ((process.env.NODE_ENV === 'development')) {
+    app.use(cors());
+}
 
 // Middlewares
 app.use('/api/v1', authRoutes);
@@ -13,5 +28,5 @@ app.use('/api/v1', authRoutes);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`API is running on port ${port}`);
+    console.log(`API is running on port ${port} - ${process.env.NODE_ENV}`);
 })
