1bc22407e247ac80e26eb19c45f7e6b03c58a973
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-10 11:18:41 +0100

    Moving controller code in controller folder

---
+++ controller/auth.controller.js
@@ -0,0 +1,7 @@
+const signup = async (req, res) => {
+    res.json({
+        data: 'This is the endpoint for registration'
+    });
+}
+
+module.exports = { signup };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,7 @@
 const express = require('express');
+const { signup } = require('../controller/auth.controller');
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-    res.json({
-        data: 'This is the endpoint for registration'
-    });
-});
+router.get('/signup', signup);
 
 module.exports = router;
