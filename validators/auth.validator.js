ac17dceb291232a2e7556b9daa70c72abbdf2c8e
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-11 23:57:29 +0100

    Validator email

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -1,5 +1,6 @@
 const { check } = require('express-validator');
 
 exports.userSignupValidator = [
-    check('name').not().isEmpty().whitelist('Name is required')
+    check('name').not().isEmpty().withMessage('Name is required'),
+    check('email').not().isEmail().withMessage('Must be a valid email address'),
 ];
