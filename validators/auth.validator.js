24a0115c243d8facda7893ce047e957c8f58c2ae
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-11 23:59:01 +0100

    Validator password

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -3,4 +3,5 @@ const { check } = require('express-validator');
 exports.userSignupValidator = [
     check('name').not().isEmpty().withMessage('Name is required'),
     check('email').not().isEmail().withMessage('Must be a valid email address'),
+    check('password').isLength().withMessage('Password must be at least 6 characters long'),
 ];
